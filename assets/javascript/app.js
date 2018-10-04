a6c1b316dac907894e8077103fd752561723cae4
mig9tx <migue.trujillo@utexas.edu>
2018-10-03 19:12:44 -0500

    psuedocode and start js

---
+++ assets/javascript/app.js
@@ -0,0 +1,33 @@
+$(document).ready(function) {
+    function initialize() {
+        var button = $"#ID"
+    }
+}
+
+//intro screen
+    //brief description of app
+    //begin or start button
+    //on click event that hides intro and displays media type picture buttons
+
+//display media
+    //TV
+    //Movie
+    //on click event that saves variable, hides media buttons and displays genre buttons
+
+//display genres
+    //drama, action, romance, horror, animated, comedy, music, documentary
+    //on click event that saves variable and hides genre
+
+//for movies, display decades for the movie types
+    //classics < 80s
+    //80s
+    //90s
+    //2000s
+    //2010s
+    //on click event that saves value, hides buttons and grabs query with the values saved
+        //throughout questionnaire
+        //It also will display the top 10 choices in descenting popularity
+            //displayed as buttons with images of the posters
+
+
+
